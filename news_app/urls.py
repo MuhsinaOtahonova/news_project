51d6441ba7b6e00e840c069eb8d3dace8b3ad696
Muhsina <muhsinahon.uzb@gmail.com>
2025-03-08 18:24:56 +0300

    add detail each news

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from .views import news_list, news_detail, homePgaeView, ContactPageView, errorPageView, HomePgaeView
-from .views import DefenseInventoryView, NationalProductionView, HumanitarianEffortsView, DefenseAviationTechnologiesView, AiTechView
+from .views import DefenseInventoryView, NationalProductionView, HumanitarianEffortsView, \
+    DefenseAviationTechnologiesView, AiTechView, NewsUpdateView, NewsDeleteView, NewsCreateView
 
 urlpatterns = [
     path('', HomePgaeView.as_view(), name = 'home_page'),
     path('news/', news_list, name = "all_news_list"),
+    path('news/create/', NewsCreateView.as_view(), name = 'news_create'),
     path('news/<slug:news>/', news_detail, name = "news_detail_page"),
     path('contact-us/', ContactPageView.as_view(), name = "contact_page"),
     path('404/', errorPageView, name = "404_page"),
+    path('news/<slug>/edit/', NewsUpdateView.as_view(), name = 'news_update'),
+    path('news/<slug>/delete/', NewsDeleteView.as_view(), name = 'news_delete'),
 
     path('defense-inventory/', DefenseInventoryView.as_view(), name = "defense_inventory_page"),
     path('national-production/', NationalProductionView.as_view(), name = "national_production_page"),
--- news_app/views.py
+++ news_app/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import News, Category 
 from .forms import ContactForm
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView,UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
 # Create your views here.
 
 def news_list(request):
@@ -140,3 +141,20 @@ class HumanitarianEffortsView(ListView):
         def get_queryset(self):
             news = self.model.published.all().filter(category__name = "Humanitarian Aid and Social Responsibility")
             return news
+
+class NewsUpdateView(UpdateView):
+        model = News
+        fields = ("title", "body", "image", "category", "status",)
+        template_name = 'crud/news_edit.html'
+
+class NewsDeleteView(DeleteView):
+        model = News
+        template_name = 'crud/news_delete.html'
+        success_url = reverse_lazy("home_page")
+
+class NewsCreateView(CreateView):
+        model = News
+        template_name = 'crud/news_create.html'
+        fields = ("title", "slug", "body", "image", "category", "status")
+
+        
